9d375c0a9211dae660a171561e23f05e35e561a0
Ramya12805 <polisettyramyasri12@gmail.com>
2024-04-04 00:50:28 +0530

    changed footer

--- src/components/Homepage/Footer.js
+++ src/components/Homepage/Footer.js
@@ -21,18 +21,15 @@ const Footer = () => {
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
-          <span>Work</span>
+        <span><a href="/" style={{color: "hsl(217, 12%, 63%)"}}>Home</a></span>
+        <span><a href="/#about" style={{color: "hsl(217, 12%, 63%)"}}>About</a></span>
+        <span><a href="/#userGuide" style={{color: "hsl(217, 12%, 63%)"}}>User Guide</a></span>
+        <span><a href="/#feedback" style={{color: "hsl(217, 12%, 63%)"}}>Feedback</a></span>
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <span><a href="https://www.amrita.edu" target="_blank" style={{color: "hsl(217, 12%, 63%)"}}>Contact Us</a></span>
+          <span><a href="/adminlogin" style={{color: "hsl(217, 12%, 63%)"}}>Admin login</a></span>
+          <span><a href="/login" style={{color: "hsl(217, 12%, 63%)"}}>User login</a></span>
         </div>
         <div className="footer-section-columns">
           <span>Terms & Conditions</span>
